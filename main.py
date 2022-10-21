274c66459c9f3b9a380c5ba9c2b8aca93e727a5a
XEROXV23 <carlos.vidal0493@gmail.com>
2022-10-20 19:08:01 -0500

    models_users

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
+
+""" NUESTRO PRIMER API - PROYECTO CLON DE TWITTER - FASTAPI - PYTHON"""
+
+# Python
+from datetime import date
+from typing import Optional
+from uuid import UUID
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    passwrod: str = Field(
+        ...,
+        min_length=8
+        )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=4,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=4,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default = None) 
+
+class Tweet(BaseModel):
+    pass
+
 
 @app.get(path="/")
 def home():
