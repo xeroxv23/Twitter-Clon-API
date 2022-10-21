056dcfb2c52b79902ae055cab3bdd355e4429181
XEROXV23 <carlos.vidal0493@gmail.com>
2022-10-21 00:01:14 -0500

    tweet_post

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ class Tweet(BaseModel):
         min_length=1,
         max_length=256
         )
-    create_at: datetime = Field(default=datetime.today())
-    update_at: Optional[datetime] = Field(default=None)
+    created_at: datetime = Field(default=datetime.today())
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
 #  Path Operations
@@ -194,8 +194,36 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a Tweet
+
+    This path operation post a tweet in the app
+
+    Parameters:
+        - Request body parameter
+            - tweet: Tweet
+
+    Returns a json with the basic tweet information:
+        tweet_id: UUID
+        content: str
+        create_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
