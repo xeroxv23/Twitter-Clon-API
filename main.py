9b134c2486b1efa7e5feaba207dc16af45962d9c
XEROXV23 <carlos.vidal0493@gmail.com>
2022-10-21 00:26:17 -0500

    Finalizamos el home, quedan pendiente las siguientes path operations(6)

--- main.py
+++ main.py
@@ -184,7 +184,22 @@ def update_a_user():
     tags=["Tweets"]
 )
 def home():
-    return {"Twitter API": "Working!"}
+    """
+    This path operations shows all the tweets in the app
+
+    Parameters:
+        -
+
+    Returns a json list with all tweets in the app with the following keys:
+        tweet_id: UUID
+        content: str
+        create_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Post a tweet
 @app.post(
