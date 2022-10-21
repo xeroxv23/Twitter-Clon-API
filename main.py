06f18c35b94be558c76818f70d856533fbefbe1a
XEROXV23 <carlos.vidal0493@gmail.com>
2022-10-20 23:33:40 -0500

    users_show_all

--- main.py
+++ main.py
@@ -103,8 +103,6 @@ def signup(user: UserRegister = Body(...)):
         f.write(json.dumps(results))
         return user
 
-
-
 ### Login a user
 @app.post(
     path="/login",
@@ -125,7 +123,22 @@ def login():
     tags=["Users"]
 )
 def show_all_users():
-    pass
+    """
+    This path operations shows all users in the app
+
+    Parameters:
+        -
+
+    Returns a json list with all users in the app with the following keys:
+        -user_id: UUID
+        -email: Emailstr
+        -first_name: str
+        -last_name: str
+        -birth_date: date
+    """
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Show a user
 @app.get(
